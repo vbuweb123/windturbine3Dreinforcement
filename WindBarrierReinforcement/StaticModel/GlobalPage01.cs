183a986953e2bff833f4c8c70649ddb6ba9ad26f
rami <hamati@rami2004@yahoo.com>
2017-12-10 20:32:14 +0200

    Adding Global Pages

---
+++ WindBarrierReinforcement/StaticModel/GlobalPage01.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindBarrierReinforcement.DataModels.NSPage01;
+
+namespace WindBarrierReinforcement.StaticModel
+{
+    public static class GlobalPage01
+    {
+        private static DataModel_Global_Formwork dataModel_Global_Formwork;
+        public static DataModel_Global_Formwork DataModel_Global_Formwork
+            => dataModel_Global_Formwork ?? (dataModel_Global_Formwork = new DataModel_Global_Formwork());
+
+        private static DataModel_Global_Coordinations_GroundPoint dataModel_Global_Coordinations_GroundPoint;
+        public static DataModel_Global_Coordinations_GroundPoint DataModel_Global_Coordinations_GroundPoint
+            => dataModel_Global_Coordinations_GroundPoint ?? (dataModel_Global_Coordinations_GroundPoint = new DataModel_Global_Coordinations_GroundPoint());
+
+        private static DataModel_Global_Materials dataModel_Global_Materials;
+        public static DataModel_Global_Materials DataModel_Global_Materials
+            => dataModel_Global_Materials ?? (dataModel_Global_Materials = new DataModel_Global_Materials());
+    }
+}
